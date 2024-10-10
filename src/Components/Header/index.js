e27528e2fa093a4a78a32a4131b341ee136bce32
manav2121 <manavkashyap6708@gmail.com>
2024-10-10 14:37:12 +0530

    second commit

---
+++ src/Components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="top-strip">
+        <div className="container">
+          <p className="mb-0 mt-0 text-center">qwerty</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
